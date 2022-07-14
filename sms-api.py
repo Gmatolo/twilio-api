628d5fd5fb8bc29e13c1c560babfae76707a6299
gmatolo <geraldmuli7@gmail.com>
2022-07-14 12:44:11 +0300

    Create sms with origin and destination

--- sms-api.py
+++ sms-api.py
@@ -6,3 +6,10 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         body='This is the ship that made the Kessel Run in fourteen parsecs?',
+         from_='+15055914607',
+         to='+254719749633'
+     )
